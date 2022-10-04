387e6e5ec8dc4d16ea62ba999c76356a4a05b87c
Igor.K <igor.k.nsk@gmail.com>
2022-10-04 16:03:54 +0700

    add table tea in db

---
+++ db/migrations/20221004084332-create-tea.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Teas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      descriptions: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.STRING,
+      },
+      imgLink: {
+        type: Sequelize.STRING,
+      }
+      // createdAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE
+      // },
+      // updatedAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE
+      // }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Teas');
+  }
+};
