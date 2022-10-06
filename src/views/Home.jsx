fc773e7a2049fce34bc8c65a8e929bf7d2a3ea10
Julia <frolovajulia1310@gmail.com>
2022-10-06 16:14:57 +0500

    styleHome

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ const Layout = require('./Layout');
 module.exports = function Home({ newUser }) {
   return (
     <Layout newUser={newUser}>
-      <h1>Hello user! {' '} {newUser}</h1>
+      <h1>Добро пожаловать в мир чая! {' '} {newUser}</h1>
       <div id="map" className="map"> </div>
       <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
     </Layout>
--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -10,6 +10,7 @@ module.exports = function Layout({ children, newUser }) {
         <title>Tea Party</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
         <link  rel="stylesheet" href="/css/style.css" />
+        <link rel="stylesheet" href="css/home.css" />
         <script defer src="/js/script.js"></script>'
         <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
       </head>
@@ -21,7 +22,6 @@ module.exports = function Layout({ children, newUser }) {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon" />
             </button>
-            {/* {variable ? some code : some other code} */}
             { newUser ? (
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
