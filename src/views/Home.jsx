570570c64c490bf9cbd7e894274c660548f3d885
Galchenko-Artem <artem.galchenko.88@gmail.com>
2022-10-07 10:17:37 +0300

    develop 3

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,8 +5,8 @@ module.exports = function Home({ newUser }) {
   return (
     <Layout newUser={newUser}>
       <h1>Hello user! {' '} {newUser}</h1>
-      <div id="map" className="map"> </div>
-      <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+      <div id="maps" className="map"> </div>
+     <script src="https://api-maps.yandex.ru/2.1/?apikey=66aa6850-5430-4a22-8e63-26a8fe081633&lang=ru_RU" type="text/javascript"></script>
     </Layout>
   );
 };
