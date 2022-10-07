8a356635eea97fd076ed7835cc1c666d64dbfd9f
Galchenko-Artem <artem.galchenko.88@gmail.com>
2022-10-07 17:51:51 +0300

    reliase 2

--- src/routes/personalRouter.js
+++ src/routes/personalRouter.js
@@ -8,6 +8,15 @@ const Personal = require('../views/Personal')
 router.get('/',  (req, res) => {
     renderTemplate(Personal, null, res);
 
+});
+ 
+router.post('/', async (req, res) => {
+    try {
+        const teas = await Tea.create({title, descriptions, location, imgLink});
+        res.redirect('/')
+    } catch (error) {
+        res.send(`Error ------> ${error}`);
+    }
 })
 
 
--- src/views/Personal.jsx
+++ src/views/Personal.jsx
@@ -6,17 +6,26 @@ module.exports = function Personal(props) {
     <Layout >
         <div class="input-group flex-nowrap">
           <span class="input-group-text" id="addon-wrapping">@</span>
-          <input type="text" class="form-control" placeholder="Название чая" aria-label="Имя пользователя" aria-describedby="addon-wrapping"/>
+          <input name="title" type="text" class="form-control" placeholder="Название чая" aria-label="Имя пользователя" aria-describedby="addon-wrapping"/>
         </div>
 
         <div class="input-group flex-nowrap">
           <span class="input-group-text" id="addon-wrapping">@</span>
-          <input type="text" class="form-control" placeholder="Описание" aria-label="Имя пользователя" aria-describedby="addon-wrapping"/>
+          <input name="descriptions" type="text" class="form-control" placeholder="Описание" aria-label="Имя пользователя" aria-describedby="addon-wrapping"/>
        </div>
 
        <div class="input-group flex-nowrap">
           <span class="input-group-text" id="addon-wrapping">@</span>
-          <input type="text" class="form-control" placeholder="Локация" aria-label="Имя пользователя" aria-describedby="addon-wrapping"/>
+          <input name="location" type="text" class="form-control" placeholder="Локация" aria-label="Имя пользователя" aria-describedby="addon-wrapping"/>
+       </div>
+
+       <div class="input-group flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">@</span>
+          <input type="text" class="form-control" placeholder="Картинка" aria-label="Имя пользователя" aria-describedby="addon-wrapping"/>
+       </div>
+
+       <div class="d-grid gap-2">
+          <button name="imgLink" class="btn btn-primary" type="button">Создать запись!!!</button>
        </div>
       
     </Layout>
