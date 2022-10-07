0b7e6220cf6feb75f1a185e126c3931721448f50
Galchenko-Artem <artem.galchenko.88@gmail.com>
2022-10-07 11:01:48 +0300

    develop 4

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,48 @@
 async function getMark(){
-    const resul = await fetch('/',{
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-    });
-    const teas = await resul.json()
+  const resul = await fetch('/',{
+      method: 'POST',
+      headers:{
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+  });
+  const teas = await resul.json()
 ymaps.ready(() => {
-    const myMap = new ymaps.Map("maps", {
-        center:[55.943233465759036,37.26191203676665],
-        zoom:2,
-        controls: [
-            'typeSelector',
-            'fullscreenControl', 
-          ]
-    },{
-        searchControlProvider: 'yandex#search',
-    });
-     teas.forEach((tea) =>{
-        const MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>',
-          );
-          const geocode =  ymaps.geocode(tea.location);
-          geocode.then((res)=>{
-            const firstGeoObject = res.geoObjects.get(0);
-            const coords = firstGeoObject.geometry.getCoordinates();
-            const mark = new ymaps.Placemark(coords, {
-              hintContent: tea.title,
-            }, {
-                iconLayout: 'default#imageWithContent',
-                iconImageHref: '/img/leaf.png',
-                iconImageSize: [24, 24],
-                iconImageOffset: [-24, -24],
-                iconContentOffset: [15, 15],
-                iconContentLayout: MyIconContentLayout,
-            });
-            mark.events.add('click', () => {
-              window.location.assign(`tea/${tea.id}`);
-            });
-            myMap.geoObjects.add(mark);
+  const myMap = new ymaps.Map("maps", {
+      center:[55.943233465759036,37.26191203676665],
+      zoom:2,
+      controls: [
+          'typeSelector',
+          'fullscreenControl', 
+        ]
+  },{
+      searchControlProvider: 'yandex#search',
+  });
+   teas.forEach((tea) =>{
+      const MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>',
+        );
+        const geocode =  ymaps.geocode(tea.location);
+        geocode.then((res)=>{
+          const firstGeoObject = res.geoObjects.get(0);
+          const coords = firstGeoObject.geometry.getCoordinates();
+          const mark = new ymaps.Placemark(coords, {
+            hintContent: tea.title,
+          }, {
+              iconLayout: 'default#imageWithContent',
+              iconImageHref: '/img/leaf.png',
+              iconImageSize: [24, 24],
+              iconImageOffset: [-24, -24],
+              iconContentOffset: [15, 15],
+              iconContentLayout: MyIconContentLayout,
           });
+          mark.events.add('click', () => {
+            window.location.assign(`tea/${tea.id}`);
+          });
+          myMap.geoObjects.add(mark);
         });
       });
-    }
-    getMark()
-    
+    });
+  }
+  getMark()
     
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,13 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function Home({ newUser, teaMark }) {
-  console.log(newUser);
-  console.log(teaMark);
+module.exports = function Home({ newUser }) {
   return (
     <Layout newUser={newUser}>
       <h1>Hello user! {' '} {newUser}</h1>
-      <div className="container">
-      <div id="map" className="map"> </div>
-        <script src="https://api-maps.yandex.ru/2.1/?apikey=66aa6850-5430-4a22-8e63-26a8fe081633&lang=ru_RU" type="text/javascript"></script>
-        <script defer src="/js/script.js"></script>
-      </div>
+      <div id="maps" className="map"> </div>
+     <script src="https://api-maps.yandex.ru/2.1/?apikey=a7006369-0322-492b-a3b1-cc11d76d3ed5&lang=ru_RU" type="text/javascript"></script>
+
     </Layout>
   );
 };
